a78b12c1ee0e49f84b8c2854a8c2e4c2c316c59b
Mark O'Brien <mark.obrien@wpengine.com>
2021-01-03 20:58:53 +0000

    Finished script to scrape records from IMDB

--- data/web_scrape_tutorial.py
+++ data/web_scrape_tutorial.py
@@ -13,7 +13,44 @@ first_movie = movie_containers[0]
 first_movie_title = first_movie.h3.a.text
 first_movie_year = first_movie.find('span', class_='lister-item-year text-muted unbold').text
 first_movie_rating = float(first_movie.strong.text)
-first_movie_mscore = first_movie.find('span', class_='metascore favorable').text
-# TODO - come back at number of votes section
+first_movie_mscore = int(first_movie.find('span', class_='metascore favorable').text)
+first_movie_votes = int(first_movie.find('span', attrs={'name': 'nv'})['data-value'])
 
-print(f'{first_movie_title} released in {first_movie_year} with rating of {first_movie_rating} and metascore of {first_movie_mscore}')
+print(
+    f'{first_movie_title} released in '
+    f'{first_movie_year} with rating of '
+    f'{first_movie_rating} and metascore of '
+    f'{first_movie_mscore} with '
+    f'{first_movie_votes} votes')
+
+
+# Actual scraping
+names = []
+years = []
+imdb_ratings = []
+metascores = []
+votes = []
+
+for container in movie_containers:
+    if container.find('div', class_='ratings-metascore') is not None:
+        names.append(container.h3.a.text)
+        years.append(container.find('span', class_='lister-item-year text-muted unbold').text)
+        imdb_ratings.append(float(container.strong.text))
+        metascores.append(int(container.find('span', class_='metascore').text))
+        votes.append(int(container.find('span', attrs={'name': 'nv'})['data-value']))
+
+import pandas as pd
+test_df = pd.DataFrame(
+    {
+        'movie': names,
+        'year': years,
+        'imdb': imdb_ratings,
+        'metascore': metascores,
+        'votes': votes
+    }
+)
+
+print(test_df.info())
+print(test_df)
+
+# TODO - start off from Script for Multiple Pages
